398c757277be0526e07cfa00dd42d6fd1eaa3d91
Danish Sharma <danishs@everstring.com>
2019-03-11 23:22:37 -0700

    firebase

--- src/Load.tsx
+++ src/Load.tsx
@@ -4,22 +4,45 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import Main from './Main';
 import store, { persistor } from './store/store';
+import * as firebase from 'firebase';
+import fb from './screens/home/settings/firebaseUser';
 
 export default class Load extends React.Component<{
 	skipLoading?: boolean
 }> {
 	
 	state = {
-		loading: true
+		loading: true,
+        authenticated:false
 	};
 	
 	async componentWillMount() {
-		await Font.loadAsync( {
-			Roboto:        require( 'native-base/Fonts/Roboto.ttf' ),
-			Roboto_medium: require( 'native-base/Fonts/Roboto_medium.ttf' )
-		} );
-		this.setState( { loading: false } );
-	}
+if (!firebase.apps.length) { firebase.initializeApp({
+		apiKey: "AIzaSyARa0qPrnusB9TsPkFA9hRRk4dFRUsEObo",
+    authDomain: "groupclock-268.firebaseapp.com",
+    databaseURL: "https://groupclock-268.firebaseio.com",
+    projectId: "groupclock-268",
+    storageBucket: "groupclock-268.appspot.com",
+    messagingSenderId: "881247202497"
+	
+}); }
+    firebase.auth().onAuthStateChanged(async (user)=>{
+    	console.log(!!user);
+    	fb.user = user;
+
+        // retrieve all keys from firebase
+        // JSON.parse()
+        // load to every store
+
+        this.setState({authenticated: true});
+    });
+	
+await Font.loadAsync( {
+            Roboto:        require( 'native-base/Fonts/Roboto.ttf' ),
+            Roboto_medium: require( 'native-base/Fonts/Roboto_medium.ttf' )
+        } );
+        this.setState( { loading: false } );
+  }
 	
 	render() {
 		if ( this.state.loading && !this.props.skipLoading )
